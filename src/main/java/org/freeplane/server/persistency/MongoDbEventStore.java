201959e69975c159773f09ab7950b58c8a738a42
Felix Natter <fnatter@gmx.net>
2017-08-27 21:33:11 +0200

    mongodb: remove deprecations, separate test and production host/db using properties

--- src/main/java/org/freeplane/server/persistency/MongoDbEventStore.java
+++ src/main/java/org/freeplane/server/persistency/MongoDbEventStore.java
@@ -1,13 +1,32 @@
 package org.freeplane.server.persistency;
 
+import org.freeplane.server.persistency.events.GenericEvent;
+import org.freeplane.server.persistency.events.GenericEventRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class MongoDbEventStore implements EventStore {
+public class MongoDbEventStore implements EventStore, InitializingBean {
+
+	private static final Logger logger = LoggerFactory.getLogger(MongoDbEventStore.class);
+	
+	@Autowired
+	private GenericEventRepository genericEventRepository;
 
 	@Override
 	public void store(String mapId, String nodeId, String contentType,
 			long version, String content) {
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		logger.info("MongoDbEventStore: creating GenericEvent!");
+		final String json = "{\"content\": \"PROD!!!\");";
+		GenericEvent genericEvent = new GenericEvent("mapId1", "nodeId1", "CORE", json);
+		genericEventRepository.save(genericEvent);
 	}
 
 }
--- src/test/java/org/freeplane/server/persistency/events/GenericEventRepositoryTest.java
+++ src/test/java/org/freeplane/server/persistency/events/GenericEventRepositoryTest.java
@@ -4,14 +4,17 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
@@ -19,13 +22,23 @@ public class GenericEventRepositoryTest {
 
 	@Configuration
 	@EnableMongoRepositories("org.freeplane.server.persistency.events")
+	@PropertySource("classpath:application.properties")
 	static class TestConfiguration {
-		public @Bean Mongo mongo() throws Exception {
-			return new Mongo("localhost");
+
+		@Value("${spring.data.mongodb.test.host}")
+		private String mongoTestHost;
+
+		@Value("${spring.data.mongodb.test.database}")
+		private String mongoTestDatabase;
+
+		@Bean
+		public Mongo mongo() throws Exception {
+			return new MongoClient(mongoTestHost);
 		}
 
-		public @Bean MongoTemplate mongoTemplate() throws Exception {
-			return new MongoTemplate(mongo(), "testdb");
+		@Bean
+		public MongoTemplate mongoTemplate() throws Exception {
+			return new MongoTemplate(mongo(), mongoTestDatabase);
 		}
 	}
 	
@@ -38,6 +51,5 @@ public class GenericEventRepositoryTest {
 		GenericEvent genericEvent = new GenericEvent("mapId1", "nodeId1", "CORE", json);
 		genericEventRepository.save(genericEvent);
 		Assert.assertEquals(json, genericEventRepository.findOne("mapId1:nodeId1:CORE").getJson());
-		//Assert.assertEquals(1, genericEventRepository.findByCompositeKey("mapId1", "nodeId1", "CORE"));
 	}
 }
