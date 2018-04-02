cb65b6d48813857202ea063bb532ec556aa0d426
Felix Natter <fnatter@gmx.net>
2018-04-02 09:36:27 +0200

    add simple WebSocket{Config,Server} for testing basic client feature

--- src/main/java/org/freeplane/server/WebSocketConfig.java
+++ src/main/java/org/freeplane/server/WebSocketConfig.java
@@ -1,24 +1,30 @@
 package org.freeplane.server;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.handler.ExceptionWebSocketHandlerDecorator;
+
+//docu:
+//http://www.devglan.com/spring-boot/spring-websocket-integration-example-without-stomp
+//https://stackoverflow.com/questions/26452903/javax-websocket-client-simple-example
+//https://www.youtube.com/watch?v=nxakp15CACY
 
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+	@Autowired
+	private WebSocketServer webSocketServer;
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/freeplane");
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new ExceptionWebSocketHandlerDecorator(webSocketServer), "/freeplane");
     }
-
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/freeplane").withSockJS();
-    }
-
 }
---
+++ src/main/java/org/freeplane/server/WebSocketServer.java
@@ -0,0 +1,44 @@
+package org.freeplane.server;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import org.freeplane.collaboration.event.batch.GenericUpdateBlockCompleted;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Component
+public class WebSocketServer extends TextWebSocketHandler {
+	
+	private static final Logger logger = LoggerFactory.getLogger(WebSocketServer.class);
+
+	@Autowired
+	private ObjectMapper objectMapper;
+	
+	List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
+	
+    @Override
+    public void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
+    	logger.info("Server received text: {}", message.getPayload());
+//    	GenericUpdateBlockCompleted serverUpdatesFinished = objectMapper.readValue(message.getPayload(), GenericUpdateBlockCompleted.class);
+//    	logger.info("Server received message: {}", serverUpdatesFinished);
+//    	for(WebSocketSession webSocketSession : sessions) {
+//    		webSocketSession.sendMessage(new TextMessage(objectMapper.writeValueAsString(serverUpdatesFinished)));
+//    	}
+    }
+
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+		//the messages will be broadcasted to all users
+		sessions.add(session);
+	}
+
+}
