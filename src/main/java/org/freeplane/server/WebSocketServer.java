017db2896d7ccac5c141985d50d6d651af8833ee
Felix Natter <fnatter@gmx.net>
2018-04-15 19:19:42 +0200

    Reply with MapCreated to freeplane client

--- src/main/java/org/freeplane/server/WebSocketServer.java
+++ src/main/java/org/freeplane/server/WebSocketServer.java
@@ -1,9 +1,18 @@
 package org.freeplane.server;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+import org.freeplane.collaboration.event.messages.ImmutableMapCreated;
+import org.freeplane.collaboration.event.messages.ImmutableMapId;
+import org.freeplane.collaboration.event.messages.ImmutableMessageId;
+import org.freeplane.collaboration.event.messages.MapCreateRequested;
+import org.freeplane.collaboration.event.messages.MapCreated;
+import org.freeplane.collaboration.event.messages.MapId;
+import org.freeplane.collaboration.event.messages.Message;
 import org.freeplane.server.persistency.MongoDbEventStore;
 import org.freeplane.server.persistency.events.GenericEvent;
 import org.slf4j.Logger;
@@ -28,14 +37,26 @@ public class WebSocketServer extends TextWebSocketHandler {
 	private MongoDbEventStore mongoDbEventStore;
 	
 	List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
+	Map<WebSocketSession,MapId> session2MapId = new HashMap<>();
 	
     @Override
     public void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
     	logger.info("Server received text: {}", message.getPayload());
+    	
+    	Message msg = objectMapper.readValue(message.getPayload(), Message.class);
+		logger.info("Message received: {}", msg);
+    	if (msg instanceof MapCreateRequested)
+    	{
+    		logger.info("MapCreateRequested received!");
+    		ImmutableMapId mapId = ImmutableMapId.of("mapIdFromServer");
+			session2MapId.put(session, mapId);
+    		MapCreated mapCreated = ImmutableMapCreated.builder()
+    				.from(msg).id(mapId).requestId(ImmutableMessageId.of("myServerMsgId"))
+    				.build();
+    		session.sendMessage(new TextMessage(objectMapper.writeValueAsString(mapCreated)));
+    	}
+    	
     	//GenericUpdateBlockCompleted serverUpdatesFinished = objectMapper.readValue(message.getPayload(), GenericUpdateBlockCompleted.class);
-    	
-    	
-    	
 //    	GenericUpdateBlockCompleted serverUpdatesFinished = objectMapper.readValue(message.getPayload(), GenericUpdateBlockCompleted.class);
 //    	logger.info("Server received message: {}", serverUpdatesFinished);
 //    	for(WebSocketSession webSocketSession : sessions) {
